9f74382da96b22a34dfc12aabeee1b43aa850564
Zaid <zaid.naom@gmail.com>
2020-12-12 21:00:37 +0100

    Fix typos in About.jsx

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -4,8 +4,8 @@ export const About = ({ title }) => {
     return (
         <div style={{ padding: 10 }}>
             <h1>{title}</h1>
-            <p>This component is writted in JSX and can be easily imported from Feliz applications.</p>
-            <p>It accepts parameters or propes like <strong style={{ color: "red" }}>title</strong> that come from F#</p>
+            <p>This component is written in JSX and can be easily imported from Feliz applications.</p>
+            <p>It accepts parameters or props like <strong style={{ color: "red" }}>title</strong> that come from F#</p>
             <p>You can learn all about Feliz <a href="https://zaid-ajaj.github.io/Feliz">here</a></p>
         </div>
     );
